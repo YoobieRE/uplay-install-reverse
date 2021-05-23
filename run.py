e5fb8bb21c06adc548da5fc022fc89a26ac32514
Charlie Laabs <charlielaabs@gmail.com>
2021-05-23 12:53:27 -0500

    Typedef stuff

--- run.py
+++ run.py
@@ -2,13 +2,14 @@ import base64
 import blackboxprotobuf
 import zlib
 from protobuf_inspector.types import StandardParser
+import json
 
 def print_sample(plaintextBytes, cipher):
     plaintext = plaintextBytes.decode()
     sample = plaintextBytes[:300]
     print(cipher + ":\n" + sample)
 
-MANIFEST_FILE = 'files/wdl_uplay_install.manifest'
+MANIFEST_FILE = 'files/wd1_uplay_install.manifest'
 PAYLOAD_START = 356
 
 manifestBytes = open(MANIFEST_FILE, mode='rb').read()
@@ -26,12 +27,18 @@ outFile = open(MANIFEST_FILE+'.unzipped', mode='wb')
 outFile.write(decompressed)
 outFile.close()
 
-message, typedef = blackboxprotobuf.protobuf_to_json(decompressed)
+typedef = json.loads(open('combined.typedef.json', mode='r').read())
+
+message, typedef = blackboxprotobuf.protobuf_to_json(decompressed, message_type=typedef)
 outFile = open(MANIFEST_FILE+'.json', mode='w')
 outFile.write(message)
+outFile.close()
+
+outFile = open('output.typedef.json', mode='w')
+outFile.write(json.dumps(typedef, indent=2))
 outFile.close()
 
 parser = StandardParser()
 with open(MANIFEST_FILE+'.unzipped', 'rb') as fh:
    output = parser.parse_message(fh, "message")
-print(output)
+# print(output)
