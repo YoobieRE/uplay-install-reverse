dd0cb1802ea8e5411ac8f697f51a2c7427e5fec2
Charlie Laabs <charlielaabs@gmail.com>
2021-05-23 18:05:25 -0500

    first shot at designing a .proto

--- run.py
+++ run.py
@@ -2,6 +2,10 @@ import base64
 import blackboxprotobuf
 import zlib
 from protobuf_inspector.types import StandardParser
+from google.protobuf.json_format import MessageToJson
+from google.protobuf.message import DecodeError
+from google.protobuf import text_format
+import protos.manifest_pb2 as Manifest
 import json
 
 def print_sample(plaintextBytes, cipher):
@@ -23,23 +27,29 @@ ciphertext = manifestBytes[PAYLOAD_START:]
 
 decompressed = zlib.decompress(ciphertext)
 
-outFile = open(MANIFEST_FILE+'.unzipped', mode='wb')
+outFile = open(MANIFEST_FILE+'.pb', mode='wb')
 outFile.write(decompressed)
 outFile.close()
 
+manifest = Manifest.Manifest()
+manifest.ParseFromString(decompressed)
+with open(MANIFEST_FILE + '.json', 'w') as jsfile:
+    actual_json_text = MessageToJson(manifest)
+    jsfile.write( actual_json_text )
+
 # Types: https://github.com/ydkhatri/blackboxprotobuf/blob/master/blackboxprotobuf/lib/types/type_maps.py#L29
-typedef = json.loads(open('combined.typedef.json', mode='r').read())
+# typedef = json.loads(open('combined.typedef.json', mode='r').read())
 
-message, typedef = blackboxprotobuf.protobuf_to_json(decompressed, message_type=typedef)
-outFile = open(MANIFEST_FILE+'.json', mode='w')
-outFile.write(message)
-outFile.close()
+# message, typedef = blackboxprotobuf.protobuf_to_json(decompressed, message_type=None, bytes_as_hex=False) # set to True when all strings are notated
+# outFile = open(MANIFEST_FILE+'.json', mode='w')
+# outFile.write(message)
+# outFile.close()
 
-outFile = open('output.typedef.json', mode='w')
-outFile.write(json.dumps(typedef, indent=2))
-outFile.close()
+# outFile = open('output.typedef.json', mode='w')
+# outFile.write(json.dumps(typedef, indent=2))
+# outFile.close()
 
-parser = StandardParser()
-with open(MANIFEST_FILE+'.unzipped', 'rb') as fh:
-   output = parser.parse_message(fh, "message")
+# parser = StandardParser()
+# with open(MANIFEST_FILE+'.pb', 'rb') as fh:
+#    output = parser.parse_message(fh, "message")
 # print(output)
